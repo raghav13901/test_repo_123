a845074130229d217aef1cc1585f1d18b79d7e47
raghav13901 <raghavkhatoria42@gmail.com>
2022-11-12 18:48:59 +0530

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRecordWebcam } from "react-record-webcam";
 
 function App() {
+  const recordWebcam = useRecordWebcam({ frameRate: 60 });
+
+  const saveFile = async () => {
+    const blob = await recordWebcam.getRecording();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +25,18 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div>
+        <p>Camera status: {recordWebcam.status}</p>
+        <button onClick={recordWebcam.open}>Open camera</button>
+        <button onClick={recordWebcam.start}>Start recording</button>
+        <button onClick={recordWebcam.stop}>Stop recording</button>
+        <button onClick={recordWebcam.retake}>Retake recording</button>
+        <button onClick={recordWebcam.download}>Download recording</button>
+        <button onClick={saveFile}>Save file to server</button>
+        <video ref={recordWebcam.webcamRef} autoPlay muted />
+        <video ref={recordWebcam.previewRef} autoPlay muted loop />
+      </div>
     </div>
   );
 }
